8ede8ecbf48fc6aa5b01510a6bd2a11e1f3ffbed
AaditGupta <mail@aaditgupta.com>
2015-09-19 20:21:17 +0800

    Add transitions to navbar

---
+++ static/script.js
@@ -0,0 +1,20 @@
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+        $('.YNCName').css('font-size', '0px');
+        $('.CanvassyName').css('font-size', '20pt');
+        $('.CanvassyName').css('margin-top', '-20px');
+        $('.CanvassyName').css('margin-bottom', '-30px');
+        $('#userName').css('font-size', '0px');
+        //$('.userSection').css('height', '0px');
+        //$('#profilePicLink').css('visibility: hidden');
+    } else {
+        $('nav').removeClass('shrink');
+        $('.YNCName').css('font-size', '15pt');
+        $('.CanvassyName').css('font-size', '50pt');
+        $('.CanvassyName').css('margin-top', '-15px');
+        $('.userSection').css('visibility', 'visible');
+        $('#profilePic').css('width', '50px');
+        $('#userName').css('font-size', '12px');
+    }
+});
