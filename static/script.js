e57bdd3b4fbdc60f01134fc2b9aca2a48aed70b6
Ross Rauber <Ross@82-21.priv23.nus.edu.sg>
2015-09-19 20:55:55 +0800

    changed animations, design for navbar

--- static/script.js
+++ static/script.js
@@ -1,20 +1,23 @@
 $(window).scroll(function () {
     if ($(document).scrollTop() > 50) {
-        $('nav').addClass('shrink');
+        //$('nav').addClass('shrink');
         $('.YNCName').css('font-size', '0px');
         $('.CanvassyName').css('font-size', '20pt');
         $('.CanvassyName').css('margin-top', '-20px');
         $('.CanvassyName').css('margin-bottom', '-30px');
         $('#userName').css('font-size', '0px');
-        //$('.userSection').css('height', '0px');
-        //$('#profilePicLink').css('visibility: hidden');
+        $('#profilePic').css('margin-top', '10px');
+        $('#profilePic').css('width', '35px');
     } else {
-        $('nav').removeClass('shrink');
+        //$('nav').removeClass('shrink');
         $('.YNCName').css('font-size', '15pt');
         $('.CanvassyName').css('font-size', '50pt');
-        $('.CanvassyName').css('margin-top', '-15px');
+        $('.CanvassyName').css('margin-top', '-5px');
+        $('.CanvassyName').css('margin-bottom', '-15px');
         $('.userSection').css('visibility', 'visible');
         $('#profilePic').css('width', '50px');
         $('#userName').css('font-size', '12px');
+        $('#profilePic').css('margin-top', '40px');
+        $('#profilePic').css('width', '50px');
     }
 });
