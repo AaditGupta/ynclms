aff592c32e196fd3aafe078edae042f7cb2a628a
Ross Rauber <Ross@Rosss-MacBook-Pro.local>
2015-09-20 12:22:12 +0800

    added mock content blocks

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 $(window).scroll(function () {
-    if ($(document).scrollTop() > 50) {
+    if ($(document).scrollTop() > 15 ) {
         //$('nav').addClass('shrink');
         $('.YNCName').css('font-size', '0px');
         $('.CanvassyName').css('font-size', '20pt');
@@ -8,16 +8,16 @@ $(window).scroll(function () {
         $('#userName').css('font-size', '0px');
         $('#profilePic').css('margin-top', '10px');
         $('#profilePic').css('width', '35px');
+        $('#navbarContainer').css('margin-bottom', '50px');
     } else {
         //$('nav').removeClass('shrink');
-        $('.YNCName').css('font-size', '15pt');
+        $('.YNCName').css('font-size', '12pt');
         $('.CanvassyName').css('font-size', '50pt');
         $('.CanvassyName').css('margin-top', '-5px');
         $('.CanvassyName').css('margin-bottom', '-15px');
-        $('.userSection').css('visibility', 'visible');
-        $('#profilePic').css('width', '50px');
         $('#userName').css('font-size', '12px');
         $('#profilePic').css('margin-top', '40px');
         $('#profilePic').css('width', '50px');
+        $('#navbarContainer').css('margin-bottom', '197px');
     }
 });
